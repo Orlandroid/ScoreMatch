2d057460ddf21be9531b6cf04174d4983e7ad7e1
Orlando1818 <lopez.orlando.gold9@gmail.com>
2021-02-15 21:34:25 -0600

    nuevos jugadores

--- app/src/main/java/com/example/scorematchstatistics/Galeria.kt
+++ app/src/main/java/com/example/scorematchstatistics/Galeria.kt
@@ -16,36 +16,46 @@ class Galeria : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
-    private val imagenesId = arrayOf(
+    private var imagenesId = arrayOf(
         R.drawable.c_amenaza,
-        R.drawable.c_arquitecto,
-        R.drawable.c_explorador,
-        R.drawable.c_guardia,
+        R.drawable.c_amenaza_roberts,
+        R.drawable.c_arquitecto_budak,
+        R.drawable.c_comandante_segura,
         R.drawable.c_horvath,
-        R.drawable.c_intruso,
+        R.drawable.c_explorador_cabrera,
+        R.drawable.c_explorador_nazari,
+        R.drawable.c_explorador_ramalho,
+        R.drawable.c_guardia_bechet,
+        R.drawable.c_guardia_davies,
+        R.drawable.c_guardia_eriksson,
+        R.drawable.c_guardia_richads,
+        R.drawable.c_infiltrador_bartosz,
+        R.drawable.c_infiltrador_wu,
+        R.drawable.c_intruso_babangida,
+        R.drawable.c_intruso_kovalchuk,
+        R.drawable.c_major,
+        R.drawable.c_marksman_faustinho,
         R.drawable.c_mazo,
-        R.drawable.c_motor,
-        R.drawable.c_productor,
-        R.drawable.c_protector,
-        R.drawable.c_velocista
+        R.drawable.c_mazo_mensah,
+        R.drawable.c_medoreador_badr,
+        R.drawable.c_medoreador_gallagher,
+        R.drawable.c_motor_nielsen,
+        R.drawable.c_motor_zukic,
+        R.drawable.c_muro_romero,
+        R.drawable.c_portero_lechner,
+        R.drawable.c_portero_schimidt,
+        R.drawable.c_portero_taylor,
+        R.drawable.c_productor_camara,
+        R.drawable.c_productor_oliva,
+        R.drawable.c_productor_santos,
+        R.drawable.c_protector_dekic,
+        R.drawable.c_protector_nacho,
+        R.drawable.c_velocista,
+        R.drawable.c_velocista_dubois,
+        R.drawable.c_velocista_goosens
     )
 
-    private val nombres = arrayOf(
-        "Amenaza",
-        "Arquitecto",
-        "Explorador",
-        "Guardia",
-        "Horvath",
-        "Intruso",
-        "Mazo",
-        "Motor",
-        "Productor",
-        "Protector",
-        "Velocista"
-    )
-
-
-    private var contador = 0
+    private lateinit var nombres:Array<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -60,6 +70,7 @@ class Galeria : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val vista = inflater.inflate(R.layout.fragment_galeria, container, false)
+        nombres=requireContext().resources.getStringArray(R.array.nombres_jugadores)
         val gridItems: RecyclerView = vista.findViewById(R.id.gridItems)
         val adaptadpr = ListaGaleria(nombres, imagenesId, requireContext())
         val gridLayout = GridLayoutManager(requireContext(), 2)
