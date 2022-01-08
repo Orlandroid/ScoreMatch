fae28c4b5c72dd6ab73d29ecc1f14b2cd25923c2
Orlandroid <lopez.orlando.gold9@gmail.com>
2022-01-08 00:47:47 -0600

    BASE DE DATOS
    Se agrego los datos
    de los niveles de los jugadores
    normales

--- app/src/main/java/com/example/scorematchstatistics/Application.kt
+++ app/src/main/java/com/example/scorematchstatistics/Application.kt
@@ -1,9 +1,6 @@
 package com.example.scorematchstatistics
 
 import android.app.Application
-import android.content.SharedPreferences
-import android.util.Log
-import android.widget.Toast
 import com.example.scorematchstatistics.data.db.DatabasePlayersProvider
 import com.example.scorematchstatistics.data.preferences.PreferencesDatabase
 import com.example.scorematchstatistics.data.repository.LocalRepository
@@ -14,32 +11,48 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltAndroidApp
-class Application : Application(){
+class Application : Application() {
 
-    @Inject lateinit var localRepository: LocalRepository
-    @Inject lateinit var preferencesDatabase: PreferencesDatabase
-    @Inject lateinit var databasePlayersProvider: DatabasePlayersProvider
+    @Inject
+    lateinit var localRepository: LocalRepository
+    @Inject
+    lateinit var preferencesDatabase: PreferencesDatabase
+    @Inject
+    lateinit var databasePlayersProvider: DatabasePlayersProvider
 
     override fun onCreate() {
         super.onCreate()
         populateDatabaseLevelPlayers()
-        Toast.makeText(this,"DATOS",Toast.LENGTH_SHORT).show()
     }
 
-    private fun populateDatabaseLevelPlayers(){
-        if(!preferencesDatabase.isPlayerDataLeveSave()){
-            CoroutineScope(Dispatchers.IO).launch{
-                localRepository.insertManyPlayer(databasePlayersProvider.getLevelsGuard())
+
+    private fun populateDatabaseLevelPlayers() {
+        if (!preferencesDatabase.isPlayerDataLeveSave()) {
+            CoroutineScope(Dispatchers.IO).launch {
+                insertNormalPlayer()
                 preferencesDatabase.savePlayerDataLevel()
             }
             return
         }
-        CoroutineScope(Dispatchers.IO).launch{
-            val level = localRepository.getAllLevelsOfPlayers()
-            level.forEach {
-                Log.w("JUGADOR",it.toString())
-            }
-        }
     }
+
+    private suspend fun insertNormalPlayer(){
+        localRepository.insertManyPlayer(databasePlayersProvider.dbLevelsGuard())
+        localRepository.insertManyPlayer(databasePlayersProvider.dbLevelsEngine())
+        localRepository.insertManyPlayer(databasePlayersProvider.dbLevelsIntruso())
+        localRepository.insertManyPlayer(databasePlayersProvider.dbLevelsInfiltrator())
+        localRepository.insertManyPlayer(databasePlayersProvider.dbLevelsArquitecto())
+        localRepository.insertManyPlayer(databasePlayersProvider.dbLevelsProducer())
+        localRepository.insertManyPlayer(databasePlayersProvider.dbLevelsExplorer())
+        localRepository.insertManyPlayer(databasePlayersProvider.dbLevelsProwler())
+        localRepository.insertManyPlayer(databasePlayersProvider.dbLevelsMenace())
+        localRepository.insertManyPlayer(databasePlayersProvider.dbLevelsSpeedsTer())
+        localRepository.insertManyPlayer(databasePlayersProvider.dbLevelsHameer())
+        localRepository.insertManyPlayer(databasePlayersProvider.dbLevelsKeeper())
+        localRepository.insertManyPlayer(databasePlayersProvider.dbLevelsGkSweeeper())
+        localRepository.insertManyPlayer(databasePlayersProvider.dbLevelsGkStopper())
+
+    }
+
 
 }
--- app/src/main/java/com/example/scorematchstatistics/data/db/DatabasePlayersProvider.kt
+++ app/src/main/java/com/example/scorematchstatistics/data/db/DatabasePlayersProvider.kt
@@ -1,57 +1,70 @@
 package com.example.scorematchstatistics.data.db
 
+import com.example.scorematchstatistics.data.dblvelplayers.NormalPlayersDb
 import com.example.scorematchstatistics.data.model.Player
 import javax.inject.Inject
 import javax.inject.Singleton
 
 
 @Singleton
-class DatabasePlayersProvider @Inject constructor() {
+class DatabasePlayersProvider @Inject constructor(private val normalPlayersDb: NormalPlayersDb) {
 
 
-    companion object {
-        const val TYPE_NORMAL = "normal"
-        const val TYPE_SUPER = "normal"
-        const val TYPE_GOLDEN = "normal"
-
-        const val GUARD = "guardia"
-        const val ENGINE = "motor"
-        const val INTRUSO = "intruder"
-        const val INFILTRADOR = "infiltrador"
-        const val ARQUITECTO = "arquitecto"
-        const val PRODUCER = "productor"
-        const val EXPLORER = "explorer"
-        const val PROWLER = "medoreador"
-        const val MENACE = "amenaza"
-        const val SPEEDSTER = "velocista"
-        const val HAMMER = "mazo"
-        const val KEEPER = "portero"
-        const val GK_SWEEPER = "sweeper"
-        const val GK_STOPPER = "stopper"
-
+    fun dbLevelsGuard():List<Player>{
+        return normalPlayersDb.getLevelsGuard()
     }
 
-    fun getLevelsGuard():List<Player>{
-        return guardLevels
+    fun dbLevelsEngine():List<Player>{
+        return normalPlayersDb.getLevelsEngine()
     }
 
+    fun dbLevelsIntruso():List<Player>{
+        return normalPlayersDb.getLevelsIntruso()
+    }
 
-    private val guardLevels = listOf(
-        Player( level = 1,name = GUARD, typeOfPlayer = TYPE_NORMAL, speed = 7, height = 5, strenght = 5,power =  3, skill = 17, resposne = 1),
-        Player( level = 2,name = GUARD, typeOfPlayer = TYPE_NORMAL, speed = 0, height = 0, strenght = 0,power =  0, skill = 0, resposne = 0),
-        Player( level = 3,name = GUARD, typeOfPlayer = TYPE_NORMAL, speed = 0, height = 0, strenght = 0,power =  0, skill = 0, resposne = 0),
-        Player( level = 4,name = GUARD, typeOfPlayer = TYPE_NORMAL, speed = 0, height = 0, strenght = 0,power =  0, skill = 0, resposne = 0),
-        Player( level = 5,name = GUARD, typeOfPlayer = TYPE_NORMAL, speed = 17, height = 9, strenght = 9,power =  6, skill = 32, resposne = 9),
-        Player( level = 6,name = GUARD, typeOfPlayer = TYPE_NORMAL, speed = 20, height = 11, strenght = 11,power =  7, skill = 35, resposne = 12),
-        Player( level = 7,name = GUARD, typeOfPlayer = TYPE_NORMAL, speed = 22, height = 12, strenght = 12,power =  8, skill = 39, resposne = 14),
-        Player( level = 8,name = GUARD, typeOfPlayer = TYPE_NORMAL, speed = 25, height = 13, strenght = 13,power =  8, skill = 43, resposne = 16),
-        Player( level = 9,name = GUARD, typeOfPlayer = TYPE_NORMAL, speed = 27, height = 14, strenght = 14,power =  9, skill = 46, resposne = 18),
-        Player( level = 10,name = GUARD, typeOfPlayer = TYPE_NORMAL, speed = 30, height = 15, strenght = 15,power =  10, skill = 50, resposne = 20),
-    )
+    fun dbLevelsInfiltrator():List<Player>{
+        return normalPlayersDb.getLevelsInfiltrator()
+    }
 
+    fun dbLevelsArquitecto():List<Player>{
+        return normalPlayersDb.getLevelsArquitecto()
+    }
 
+    fun dbLevelsProducer():List<Player>{
+        return normalPlayersDb.getLevelsProducer()
+    }
 
+    fun dbLevelsExplorer():List<Player>{
+        return normalPlayersDb.getLevelsExplorer()
+    }
 
+    fun dbLevelsProwler():List<Player>{
+        return normalPlayersDb.getLevelsProwler()
+    }
+
+    fun dbLevelsMenace():List<Player>{
+        return normalPlayersDb.getLevelsMenace()
+    }
+
+    fun dbLevelsSpeedsTer():List<Player>{
+        return normalPlayersDb.getLevelsSpeedsTer()
+    }
+
+    fun dbLevelsHameer():List<Player>{
+        return normalPlayersDb.getLevelsHameer()
+    }
+
+    fun dbLevelsKeeper():List<Player>{
+        return normalPlayersDb.getLevelsKeeper()
+    }
+
+    fun dbLevelsGkSweeeper():List<Player>{
+        return normalPlayersDb.getLevelsGkSweeper()
+    }
+
+    fun dbLevelsGkStopper():List<Player>{
+        return normalPlayersDb.getLevelsGkStopper()
+    }
 
 
 }
